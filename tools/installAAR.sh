48e5f546bdd3ecc6d047ca0ddbb1ce50eb4ed203
Youngdoo Lee <z.youngdoo.lee@gree.net>
2021-02-05 12:04:29 +0900

    update version 2.1.0

--- tools/installAAR.sh
+++ tools/installAAR.sh
@@ -2,7 +2,7 @@
 
 GROUP=com.glossom
 ARTIFACT=glossom-ads-android
-VERSION=2.0.0
+VERSION=2.1.0
 PACKAGING=aar
 
 mvn clean dependency:copy-dependencies -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION -Dtype=$PACKAGING -U
